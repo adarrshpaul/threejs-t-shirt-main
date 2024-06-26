53a20044d8a5e0ed4b7539a0eae7cb17bc474498
adarrsh_dev <adarrshdeveloper@gmail.com>
2024-06-26 09:30:57 +0530

    added emebeded compatability

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -7,13 +7,13 @@ import CameraRig from "./CameraRig";
 
 const CanvasModel = () => {
   return (
-<Canvas
+    <Canvas
       shadows
-      camera={{ position: [0, 0, 5], fov: 100 }} // Increase the field of view (fov) for a wider view
+      camera={{ position: [0, 0, 1], fov: 50 }} // Increase the field of view (fov) for a wider view
       gl={{ preserveDrawingBuffer: true }}
       className="w-full max-w-full h-full transition-all ease-in"
     >
-      <ambientLight intensity={0.6} />
+      <ambientLight intensity={0.2} />
       <Environment preset="studio" background />
 
       <CameraRig>
@@ -22,13 +22,13 @@ const CanvasModel = () => {
           <Shirt />
         </Center>
         <TrackballControls
-        enableZoom={true}
-        enableRotate={true}
-  minAzimuthAngle={-Math.PI / 4}
-  maxAzimuthAngle={Math.PI / 4}
-  minPolarAngle={Math.PI / 6}
-  maxPolarAngle={Math.PI - Math.PI / 6}
-/>
+          enableZoom={true}
+          enableRotate={true}
+          minAzimuthAngle={-Math.PI / 4}
+          maxAzimuthAngle={Math.PI / 4}
+          minPolarAngle={Math.PI / 6}
+          maxPolarAngle={Math.PI - Math.PI / 6}
+        />
       </CameraRig>
 
     </Canvas>
